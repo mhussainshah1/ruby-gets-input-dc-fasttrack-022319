3ddb6da37007baeedff30acc2052d3f4d87b29e5
Muhammad Shah <mhussainshah79@gmail.com>
2019-02-24 06:31:53 +0000

    Automatically backed up by Learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,3 +1,8 @@
-def greeting
+def greeting(name)
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
 end
+
+puts "enter your name"
+input = gets.strip
+
+puts greeting(input)
